5e98d99f521e61b5b557b0494eabc6892bb57b40
TRUNGNGHIA <ltn23591@gmail.com>
2024-11-07 01:55:44 +0700

    fix bug responsive

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,16 +1,27 @@
 const dark = document.querySelector(".control__dark");
 const light = document.querySelector(".control__light");
 const body = document.querySelector("body");
-const imgLogo = document.querySelector(".logo img");
+const imgLogo = document.querySelectorAll(".logo img");
+const imgMenu = document.querySelector(".menu__tablet-wrap .logo__title img");
+
 dark.addEventListener("click", () => {
     body.classList.add("dark-mode");
     light.style.display = "block";
     dark.style.display = "none";
-    imgLogo.style.filter = "var(--icon-filter)";
+    imgLogo.forEach((item) => {
+        item.style.filter = "var(--icon-filter)";
+    });
+    footerLogo.style.filter = "var(--icon-filter)";
+    imgMenu.style.filter = "var(--icon-filter)";
 });
+
 light.addEventListener("click", () => {
     body.classList.remove("dark-mode");
     light.style.display = "none";
     dark.style.display = "block";
-    imgLogo.style.filter = "none";
+    imgLogo.forEach((item) => {
+        item.style.filter = "none";
+    });
+    imgMenu.style.filter = "none";
+    footerLogo.style.filter = "none";
 });
